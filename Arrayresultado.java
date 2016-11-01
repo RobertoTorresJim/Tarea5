b4c1bc51bd918d3cf27d3ceead041e875aadab3c
Roberto Torres <Roberto Torres>
2016-11-01 10:26:26 -0600

    primer commit

---
+++ Arrayresultado.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Arrayresultado {
+	public LinkedList<Integer> array;
+	public int op;
+
+	
+}
---
+++ Tarea5.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Tarea5 {
+
+	
+	public Arrayresultado Quicksort(LinkedList<Integer> array){
+		Arrayresultado result = new Arrayresultado();
+		int n = array.size();
+		if(n < 1){
+			result.array = array;
+			result.op = 1;
+			return result;
+		}
+		int centro = n/2;
+		int pivote = array.get(centro);
+		array.remove(centro);
+		LinkedList<Integer> mayores;
+		LinkedList<Integer> menores;
+		
+		for(int i = 0; i < n; i++){
+			if (array.)
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
